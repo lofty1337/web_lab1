9da7fadb5a16defeef958a48b5b67e038f9eafb1
Alex <90724908+lofty1337@users.noreply.github.com>
2023-05-31 23:36:57 +0300

    lab8
    чистый код

--- code/index.php
+++ code/index.php
@@ -1,11 +1,11 @@
 <?php
+
 require_once 'vendor/autoload.php';
 
 use Google\Client;
 use Google\Service\Sheets;
 
 session_start();
-
 
 function getClient()
 {
@@ -18,41 +18,45 @@ function getClient()
     return $client;
 }
 
+function addAnnouncement($service)
+{
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $email = $_POST['email'];
+        $category = $_POST['category'];
+        $title = $_POST['title'];
+        $text = $_POST['text'];
 
-$client = getClient();
+        $values = [
+            [$email, $category, $title, $text]
+        ];
 
+        $spreadsheetId = '12tjKabprR0E6_NlX6KMySLpHbWQfwBt-dkftdEfjIlk';
+        $range = 'qwe!A1:D1';
 
-$service = new Sheets($client);
+        $requestBody = new Google_Service_Sheets_ValueRange([
+            'values' => $values
+        ]);
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email = $_POST['email'];
-    $category = $_POST['category'];
-    $title = $_POST['title'];
-    $text = $_POST['text'];
-
-
-    $values = [
-        [$email, $category, $title, $text]
-    ];
-
-
-    $spreadsheetId = '12tjKabprR0E6_NlX6KMySLpHbWQfwBt-dkftdEfjIlk';
-    $range = 'qwe!A1:D1';
-
-
-    $requestBody = new Google_Service_Sheets_ValueRange([
-        'values' => $values
-    ]);
-
-    $response = $service->spreadsheets_values->append($spreadsheetId, $range, $requestBody, [
-        'valueInputOption' => 'RAW'
-    ]);
+        $response = $service->spreadsheets_values->append($spreadsheetId, $range, $requestBody, [
+            'valueInputOption' => 'RAW'
+        ]);
+    }
 }
 
-$spreadsheetId = '12tjKabprR0E6_NlX6KMySLpHbWQfwBt-dkftdEfjIlk';
-$range = 'qwe!A:D';
-$response = $service->spreadsheets_values->get($spreadsheetId, $range);
-$values = $response->getValues();
+function getAnnouncements($service)
+{
+    $spreadsheetId = '12tjKabprR0E6_NlX6KMySLpHbWQfwBt-dkftdEfjIlk';
+    $range = 'qwe!A:D';
+    $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+    $values = $response->getValues();
+    return $values;
+}
+
+$client = getClient();
+$service = new Sheets($client);
+
+addAnnouncement($service);
+$values = getAnnouncements($service);
 
 ?>
 
@@ -64,52 +68,52 @@ $values = $response->getValues();
 </head>
 
 <body>
-<h1>Доска объявлений</h1>
+    <h1>Доска объявлений</h1>
 
-<form method="POST">
-    <label>Email:</label>
-    <input type="email" name="email" required><br><br>
+    <form method="POST">
+        <label>Email:</label>
+        <input type="email" name="email" required><br><br>
 
-    <label>Категория:</label>
-    <select name="category" required>
-        <option value="">Выберите категорию</option>
-        <option value="category1">Категория 1</option>
-        <option value="category2">Категория 2</option>
-        <option value="category3">Категория 3</option>
-    </select><br><br>
+        <label>Категория:</label>
+        <select name="category" required>
+            <option value="">Выберите категорию</option>
+            <option value="category1">Категория 1</option>
+            <option value="category2">Категория 2</option>
+            <option value="category3">Категория 3</option>
+        </select><br><br>
 
-    <label>Заголовок объявления:</label>
-    <input type="text" name="title" required><br><br>
+        <label>Заголовок объявления:</label>
+        <input type="text" name="title" required><br><br>
 
-    <label>Текст объявления:</label><br>
-    <textarea name="text" required></textarea><br><br>
+        <label>Текст объявления:</label><br>
+        <textarea name="text" required></textarea><br><br>
 
-    <button type="submit">Добавить</button>
-</form>
+        <button type="submit">Добавить</button>
+    </form>
 
-<br>
+    <br>
 
-<h2>Объявления:</h2>
-<?php if (!empty($values)): ?>
-    <table border="1">
-        <tr>
-            <th>Email</th>
-            <th>Категория</th>
-            <th>Заголовок объявления</th>
-            <th>Текст объявления</th>
-        </tr>
-        <?php foreach ($values as $row): ?>
+    <h2>Объявления:</h2>
+    <?php if (!empty($values)): ?>
+        <table border="1">
             <tr>
-                <td><?= $row[0] ?></td>
-                <td><?= $row[1] ?></td>
-                <td><?= $row[2] ?></td>
-                <td><?= $row[3] ?></td>
+                <th>Email</th>
+                <th>Категория</th>
+                <th>Заголовок объявления</th>
+                <th>Текст объявления</th>
             </tr>
-        <?php endforeach ?>
-    </table>
-<?php else: ?>
-    <p>Объявлений пока нет.</p>
-<?php endif ?>
+            <?php foreach ($values as $row): ?>
+                <tr>
+                    <td><?= $row[0] ?></td>
+                    <td><?= $row[1] ?></td>
+                    <td><?= $row[2] ?></td>
+                    <td><?= $row[3] ?></td>
+                </tr>
+            <?php endforeach ?>
+        </table>
+    <?php else: ?>
+        <p>Объявлений пока нет.</p>
+    <?php endif ?>
 </body>
 
 </html>
